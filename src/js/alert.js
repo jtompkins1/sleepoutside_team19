208a12dd5c85bf6115dc4fd63b7ae08a7a7eb213
jtompkins1 <122328059+jtompkins1@users.noreply.github.com>
2025-01-18 17:39:51 -0600

    modify alert.js to look for alerts in the json and display if found

--- src/js/alert.js
+++ src/js/alert.js
@@ -1,11 +1,38 @@
 export default class alert {
-    constructor({ message, background, color }) {
-        this.message = message;
-        this.background = background;
-        this.color = color;
+
+    constructor() {
+        //initialize empty array
+        this.alerts = [];
     }
 
-    show() {
-        console.log(`%c${this.message}`, `background: ${this.background}; color: ${this.color}`);
+    // Read and parse JSON file
+    async loadAlerts() {
+        try {
+            const response = await fetch('./alerts.json');
+            const data = await response.json();
+            this.alerts = data;
+        } catch (err) {
+            console.error('Error reading alerts.json:', err);
+        }
     }
+
+    //display alerts
+    show() {
+        //If alerts are found create a <section class="alert-list> element then  loop through the results
+        if (this.alerts.length > 0) {
+            const alertList = document.createElement('section');
+            alertList.className = 'alert-list';
+
+            this.alerts.forEach(alert => {
+                // build a <p> for each alert and apply the background and foreground colors to it specified in the alert.json file
+                const p = document.createElement('p');
+                p.textContent = alert.message;
+                p.style.backgroundColor = alert.backgroundColor;
+                p.style.color = alert.foregroundColor;
+                alertList.appendChild(p);
+            });
+        }
+    }
+
+
 }
